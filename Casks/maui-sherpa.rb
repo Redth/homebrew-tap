32e647398bf3a335d68e35f360792a9e04f676a8
Jonathan Dick <jondick@gmail.com>
2026-02-09 20:16:50 -0500

    Fix URL for MAUI Sherpa download

--- Casks/maui-sherpa.rb
+++ Casks/maui-sherpa.rb
@@ -2,7 +2,7 @@ cask "maui-sherpa" do
   version "0.2.0"
   sha256 "40f73ee9c5257f2423e3099485a0a07f78527e69013c8f8ffb09b710e4dceaea"
 
-  url "https://github.com/Redth/MAUI.Sherpa/releases/download/v#{version}/MauiSherpa-MacCatalyst.zip"
+  url "https://github.com/Redth/MAUI.Sherpa/releases/download/v#{version}/MAUI-Sherpa.zip"
   name "MAUI Sherpa"
   desc "Desktop app for managing .NET MAUI developer tools"
   homepage "https://github.com/Redth/MAUI.Sherpa"
